f05c363a582083860c9a141f53dd322426df15f8
Stefano Mancini <stefano.mancini@devinterface.com>
2014-06-03 12:13:02 +0200

    added indexable?

--- lib/generators/spree/sunspot/install/templates/config/initializers/spree_sunspot.rb
+++ lib/generators/spree/sunspot/install/templates/config/initializers/spree_sunspot.rb
@@ -2,9 +2,12 @@
 # Any fields you wish to add text or attribute indexes for should be defined here.
 # See the Sunspot README for more info: https://github.com/sunspot/sunspot
 Spree::Sunspot::Setup.configure do
-  searchable :auto_index => true, :auto_remove => true do
-    text :name, :boost => 2.0
-    text :description, :boost => 1.2
+  searchable :if => :indexable? do
+    text :name, :boost => 2.0, :more_like_this => true
+    text :description, :boost => 1.2, :more_like_this => true
+    text :taxon_names_text do
+      taxons.collect{|t| t.self_and_ancestors.map(&:name) }.flatten.join(',')
+    end
     time :available_on
     integer :taxon_ids, :references => Spree::Taxon, :multiple => true do
       taxons.collect{|t| t.self_and_ancestors.map(&:id) }.flatten
@@ -26,4 +29,9 @@ Spree::Sunspot::Setup.configure do
     #   t.name unless t.nil?
     # end
   end
+
+  def indexable?
+    available?
+  end
+
 end
